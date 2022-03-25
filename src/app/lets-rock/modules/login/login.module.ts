4c417a90228d4d677548a4a8866ad0aee9ef9cc9
Artikunazo <artik16032011@gmail.com>
2022-03-24 22:34:46 -0700

    [IMP] Add form lodgin component

--- src/app/lets-rock/modules/login/login.module.ts
+++ src/app/lets-rock/modules/login/login.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
